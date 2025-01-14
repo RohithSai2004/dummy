7a9e1949e4b2accdcb5ebf0d96c9682987d6a526
RohithSai2004 <rohitkanna12@gmail.com>
2025-01-14 12:16:44 +0530

    Initial commit with files

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Newsapp from './components/NewsApp';
+
+function App() {
+  return (
+    <div>
+      <Newsapp/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App"; // Importing the main App component
+// Importing global styles
+
+// Rendering the App component into the root element
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
